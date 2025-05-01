22caf5dc380cac8d3f22cbb3cb76b8d493f9322f
Mukul_Dev <90556544+mukulkumar-dev@users.noreply.github.com>
2025-05-01 19:51:30 +0530

    Time: 8 ms (61.26%) | Memory: 42.7 MB (69.83%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']++;
+        }
+        if (matches(s1Count, s2Count)) return true;
+
+        for (int i = s1.length(); i < s2.length(); i++) {
+            s2Count[s2.charAt(i) - 'a']++; 
+            s2Count[s2.charAt(i - s1.length()) - 'a']--; 
+
+            if (matches(s1Count, s2Count)) return true;
+        }
+        return false;
+    }
+
+    private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+}
