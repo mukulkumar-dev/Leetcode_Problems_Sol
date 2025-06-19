79671f6da486a041a1a6cd41b20263b2af9663ac
Mukul_Dev <90556544+mukulkumar-dev@users.noreply.github.com>
2025-06-19 09:39:22 +0530

    Time: 5 ms (30.84%) | Memory: 44.8 MB (88.61%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,83 @@
+class Solution {
+    public void saveplace(char[][] board, List<List<String>> allBoards){
+        String row="";
+        List<String> newBoard= new ArrayList<>();
+
+        for(int i=0;i<board.length;i++){
+            row="";
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j]=='Q')
+                    row +='Q';
+                else
+                    row +='.';
+            }
+            newBoard.add(row);
+        }
+        allBoards.add(newBoard);
+    }
+
+    public void pointer (char[][] board, List<List<String>> allBoards, int col){
+        if(col==board.length){
+            saveplace(board, allBoards);
+            return;
+        }
+        for(int row=0; row<board.length; row++) {
+           if(isSafe(row, col, board)) {
+               board[row][col] = 'Q';
+               pointer(board, allBoards, col+1);
+               board[row][col] = '.';
+           }
+       }
+    }
+
+    public boolean isSafe(int row, int col, char[][] board) {
+        for(int j=0; j<board.length; j++) {
+           if(board[row][j] == 'Q') {
+               return false;
+           }
+       }
+      
+       for(int i=0; i<board.length; i++) {
+           if(board[i][col] == 'Q') {
+               return false;
+           }
+       }
+      
+       int r = row;
+       for(int c=col; c>=0 && r>=0; c--, r--) {
+           if(board[r][c] == 'Q') {
+               return false;
+           }
+       }
+      
+       r = row;
+       for(int c=col; c<board.length && r>=0; r--, c++) {
+           if(board[r][c] == 'Q') {
+               return false;
+           }
+       }
+    
+       r = row;
+       for(int c=col; c>=0 && r<board.length; r++, c--) {
+           if(board[r][c] == 'Q') {
+               return false;
+           }
+       }
+      
+       for(int c=col; c<board.length && r<board.length; c++, r++) {
+           if(board[r][c] == 'Q') {
+               return false;
+           }
+       }
+      
+       return true;
+   }
+  
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> allBoards = new ArrayList<>();
+        char[][] board = new char[n][n];
+
+        pointer(board, allBoards,0);
+        return allBoards;
+    }
+}
