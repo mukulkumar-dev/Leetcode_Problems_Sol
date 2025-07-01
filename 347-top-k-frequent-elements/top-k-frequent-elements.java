46b402dbec5de3073686ef1dcc5572b7c209a990
Mukul_Dev <90556544+mukulkumar-dev@users.noreply.github.com>
2025-07-01 12:11:37 +0530

    Time: 11 ms (92.43%) | Memory: 50.4 MB (5.09%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,19 +1,30 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> mp=new HashMap<>();
-        for(int n:nums){
-            mp.put(n,mp.getOrDefault(n,0)+1);
+        HashMap<Integer,Integer> hp=new HashMap<>();
+        for(int i:nums){
+            hp.put(i,hp.getOrDefault(i,0)+1);
         }
-        PriorityQueue<Map.Entry<Integer,Integer>> pq =new PriorityQueue<>((a,b) ->
-        Integer.compare(b.getValue(),a.getValue())
-        ); 
-        for(Map.Entry<Integer, Integer> enter:mp.entrySet()){
-            pq.offer(enter);
+        List<Integer>[] list=new List[nums.length+1];
+        for(int i=0;i<=nums.length;i++){
+            list[i]=new ArrayList<>();
         }
-        int [] res=new int[k];
+        for(int i:hp.keySet()){
+            int freq=hp.get(i);
+            list[freq].add(i);
+        }
+        List<Integer> result=new ArrayList<>();
+        for(int i=list.length-1;i>=0 && result.size()<k ; i--){
+            for(int num:list[i]){
+                result.add(num);
+                if(result.size()==k){
+                    break;
+                }
+            }
+        }
+        int[] ans=new int[k];
         for(int i=0;i<k;i++){
-            res[i]=Objects.requireNonNull(pq.poll().getKey());
+            ans[i]=result.get(i);
         }
-        return res;
+        return ans;
     }
 }
