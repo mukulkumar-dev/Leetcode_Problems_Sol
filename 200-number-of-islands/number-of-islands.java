acca00ecc03b89724b20c2d37c1d84226e7a2abf
Mukul_Dev <90556544+mukulkumar-dev@users.noreply.github.com>
2025-07-24 22:29:58 +0530

    Time: 3 ms (83.01%) | Memory: 49.5 MB (71.97%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,7 +3,6 @@ class Solution {
         if (grid == null || grid.length == 0) {
             return 0;
         }
-        
         int numIslands = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
