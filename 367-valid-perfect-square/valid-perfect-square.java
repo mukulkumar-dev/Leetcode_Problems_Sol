097a851b5f5adbfed8cd44faf6969f57facc5423
Mukul_Dev <90556544+mukulkumar-dev@users.noreply.github.com>
2025-08-08 22:35:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (67.15%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long low=1;
+        long high=num;
+
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            if(mid*mid<num){
+                low=mid+1;
+            }else if(mid*mid>num){
+                high=mid-1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
