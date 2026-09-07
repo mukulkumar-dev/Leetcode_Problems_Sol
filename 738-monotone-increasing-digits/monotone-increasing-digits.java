48e207a918789cf582c363845b017a58c1e3e41d
Mukul_Dev <90556544+mukulkumar-dev@users.noreply.github.com>
2026-09-08 01:15:52 +0530

    Time: 1 ms (90.14%) | Memory: 42.1 MB (79.58%) - LeetSync

---
+++ 738-monotone-increasing-digits/monotone-increasing-digits.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int monotoneIncreasingDigits(int n) {
+        int position;
+        int digitInTheNextPosition;
+        while ((position = getThePositionNotSatisfied(n)) != -1) {
+            digitInTheNextPosition = ((int) (n / Math.pow(10, position - 1))) % 10;
+            n -= Math.pow(10, position - 1) * (digitInTheNextPosition + 1);
+            n -= n % Math.pow(10, position);
+            n += Math.pow(10, position) - 1;
+        }
+        return n;
+    }
+    public int getThePositionNotSatisfied(int n) {
+        int k = 10;
+        int position = 0;
+        while (n > 0) {
+            if (k < n % 10) {
+                return position;
+            } else {
+                k = n % 10;
+                n /= 10;
+                position++;
+            }
+        }
+        return -1;
+    }
+}
